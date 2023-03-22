157143505837df202360bbc3a2f5902477980efe
GeorgeDeyneka <zhoradeynecka@gmail.com>
2023-03-23 01:02:50 +0200

    Change styles for calendar, add days of month

--- src/components/CalendarGrid.vue
+++ src/components/CalendarGrid.vue
@@ -1,26 +1,50 @@
 <script>
-import { subMonths, addMonths, eachDayOfInterval, format } from "date-fns";
+import {
+  startOfMonth,
+  endOfMonth,
+  subWeeks,
+  subMonths,
+  addMonths,
+  eachDayOfInterval,
+  format,
+} from "date-fns";
+import IconArrowLeft from "./icons/IconArrowLeft.vue";
+import IconArrowRight from "./icons/IconArrowRight.vue";
 
 export default {
+  components: { IconArrowLeft, IconArrowRight },
   data() {
     return {
-      startOfWeek: new Date(1970, 0, 5),
-      endOfWeek: new Date(1970, 0, 11),
-
+      monday: new Date(1970, 0, 5),
+      sunday: new Date(1970, 0, 11),
       currentMonth: new Date(),
-
-      days: [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-      ],
     };
   },
 
   computed: {
     daysOfWeek() {
       return eachDayOfInterval({
-        start: this.startOfWeek,
-        end: this.endOfWeek,
+        start: this.monday,
+        end: this.sunday,
+      });
+    },
+
+    startOfMonth() {
+      return startOfMonth(this.currentMonth);
+    },
+
+    endOfMonth() {
+      return endOfMonth(this.currentMonth);
+    },
+
+    monthPrevWeek() {
+      return subWeeks(this.startOfMonth, 1);
+    },
+
+    countDaysInMonth() {
+      return eachDayOfInterval({
+        start: this.startOfMonth,
+        end: this.endOfMonth,
       });
     },
   },
@@ -45,19 +69,39 @@ export default {
   <main class="container">
     <div class="calendar">
       <div class="calendar-header">
-        <div class="calendar-header__button" @click="prevMonth">&lt;</div>
-        <div class="calendar-header__title">
+        <div class="calendar-header__months">
           {{ formatDate(currentMonth, "MMMM yyyy") }}
         </div>
-        <div class="calendar-header__button" @click="nextMonth">&gt;</div>
-      </div>
-      <div class="calendar-week-wrapper">
-        <div v-for="day in daysOfWeek" :key="day" class="calendar-week">
-          {{ formatDate(day, "E") }}
+
+        <div class="calendar-header__buttons">
+          <div class="calendar-header__button" @click="prevMonth">
+            <icon-arrow-left />
+          </div>
+          <div class="calendar-header__button" @click="nextMonth">
+            <icon-arrow-right />
+          </div>
         </div>
       </div>
+      <div class="calendar-subheader__week">
+        <div
+          v-for="day in daysOfWeek"
+          :key="day"
+          class="calendar-subheader__week-day"
+        >
+          <strong>
+            {{ formatDate(day, "E") }}
+          </strong>
+        </div>
+      </div>
+
       <div class="calendar-grid">
-        <div v-for="day in days" :key="day" class="calendar-day">{{ day }}</div>
+        <div
+          v-for="day in countDaysInMonth"
+          :key="day"
+          class="calendar-grid__day"
+        >
+          {{ formatDate(day, 'd') }}
+        </div>
       </div>
     </div>
   </main>
@@ -71,61 +115,79 @@ export default {
   max-width: 1024px;
   font-family: Arial, sans-serif;
 }
+
 .calendar {
   margin: 0 auto;
   width: 600px;
-  border: 1px solid #ccc;
+  border: 1px solid #303030;
   border-radius: 5px;
   overflow: hidden;
 
-  &-week-wrapper {
-    display: grid;
-    gap: 1px;
-    grid-template-columns: repeat(7, 1fr);
-  }
-
-  &-week {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .calendar-header {
+  &-header {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: #f0f0f0;
-    padding: 10px;
+    padding: 10px 20px;
+    background-color: #2d2e34;
+    color: #b6b6b6;
     font-weight: bold;
 
-    .calendar-header__button {
-      cursor: pointer;
-      font-size: 20px;
+    &__buttons {
+      display: flex;
+      gap: 20px;
     }
 
-    .calendar-header__title {
+    &__button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+    }
+
+    &__months {
       text-align: center;
       font-size: 18px;
     }
   }
 
-  .calendar-grid {
+  &-subheader {
+    &__week {
+      display: grid;
+      gap: 1px;
+      padding: 0 5px;
+      grid-template-columns: repeat(7, 1fr);
+      background-image: linear-gradient(to right, #5979b3, #c2cbde);
+    }
+
+    &__week-day {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #101010;
+    }
+  }
+
+  &-grid {
     display: grid;
     grid-template-columns: repeat(7, 1fr);
     grid-template-rows: repeat(5, 1fr);
     gap: 1px;
-    background-color: #fff;
+    background-color: #1a1b22;
+    padding: 1px;
 
-    .calendar-day {
+    &__day {
       display: flex;
       justify-content: center;
       align-items: center;
       font-size: 16px;
       cursor: pointer;
       height: 60px;
+      background-color: #2d2e34;
+      border-radius: 4px;
+      color: #d1d1d1;
 
       &:hover {
-        background-color: #f0f0f0;
+        background-color: #494d52;
       }
     }
   }
---
+++ src/components/icons/IconArrowRight.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg class="arrow">
+    <use href="../../assets/sprite.svg#icon-arrow-right"></use>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.arrow {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // Add mixin
+  width: 20px;
+  height: 20px;
+  stroke: #b6b6b6;
+}
+</style>
