52200935300d68a04e8c697060b80cfd34953efc
GeorgeDeyneka <zhoradeynecka@gmail.com>
2024-06-18 23:38:07 +0300

    Changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/VueCalendar/",
+  base: "/valendar-vue/",
   plugins: [vue()],
   resolve: {
     alias: {
